b555373b8c3fbcd6307742c3e8221e03a861b16d
OBess <dima.suschenko03@gmail.com>
2021-06-08 13:11:59 +0300

    Removed constructor and made field 'level' static

---
+++ include/loggers/logger.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+// C++
+#include <iostream>
+
+class logger
+{
+public:
+   virtual ~logger() = default;
+
+   void Log(const std::string &message)
+   {
+      this->open();
+      this->write(message);
+      this->close();
+   }
+
+protected:
+   virtual void open() = 0;
+   virtual void write(const std::string &message) = 0;
+   virtual void close() = 0;
+};
+
+#endif //LOGGER_HPP
